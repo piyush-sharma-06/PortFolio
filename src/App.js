5de9e039e5cb62f335b6b4761bd1d0babedd23d1
Piyush Sharma <ps5130125@gmail.com>
2023-09-19 22:28:12 +0530

    added all routes files and tailwing css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import ContactMe from './routes/ContactMe';
+import Project from './routes/Project';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<ContactMe />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/project' element={<Project />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/ContactMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactMe(){
+    return (
+        <>
+        <div>contact</div>
+        </>
+    );
+}
+
+export default ContactMe;
